394b666a022557b46c8f7c4c1e4bb196f7d547b8
SimOnejin <134303424+SimOnejin@users.noreply.github.com>
2024-12-06 14:25:37 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,30 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installing.');
-  });
-  
   self.addEventListener('fetch', (event) => {
     console.log('Fetching:', event.request.url);
+  });
+  
+  self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
+
+    event.waitUntil(
+      caches.open('v2').then((cache) => {
+        return cache.addAll([
+          '/index.html',
+          '/script.js',
+          '/manifest.json',
+          '/language.js',
+        ]);
+      })
+    );
+  });
+  
+  // 오래된 캐시 삭제
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.filter((cacheName) => cacheName !== 'v2').map((cacheName) => caches.delete(cacheName))
+        );
+      })
+    );
   });
   
